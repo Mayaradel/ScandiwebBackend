e4cabba627b51e9cb5e6df87a1ff9f0f6e747b9a
Mayar Adel <48599480+Mayaradel@users.noreply.github.com>
2023-11-23 02:07:39 +0200

    Update main.php

--- main.php
+++ main.php
@@ -11,7 +11,7 @@ require_once 'Furniture.php';
 
 
 
-header("Access-Control-Allow-Origin: http://localhost:3000");
+header("Access-Control-Allow-Origin:*");
 header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
 
